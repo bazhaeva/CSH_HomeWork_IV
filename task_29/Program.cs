1ffc808648e3ab1d3401508d957a48c6d11d9ad7
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-25 22:52:56 +0300

    Задача 29

---
+++ task_29/Program.cs
@@ -0,0 +1,41 @@
+﻿int length = UserNumber("Введите размер массива: ");
+int[] userArray = new int[length];
+FillArray(length);
+PrintArray(userArray);
+
+
+//=======================Методы============================
+//-----------------------Заполнение массива
+void FillArray(int length)
+{
+        for(int i = 0; i<length; i++)
+    {
+         userArray[i]= UserNumber($"Введите элемент массива {i}: ");
+    }
+}
+
+//-----------------------Вывод массива
+void PrintArray(int[] array)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write($"]");
+}
+
+//----------------Ввод данных
+static int UserNumber(string messageToUser)
+{
+    Console.Write(messageToUser);
+    try
+    {
+        return int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
