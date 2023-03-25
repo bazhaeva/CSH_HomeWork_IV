292c6f5334c88574a9adf55497a5cf49bc9ab7e8
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-25 22:49:04 +0300

    Задача 27

---
+++ task_27/Program.cs
@@ -0,0 +1,40 @@
+﻿int numberA = UserNumber("Введите число: ");
+
+int result = Method1(numberA);
+
+Console.WriteLine($"{numberA} -> {result}");
+
+//=============================Методы==================
+
+//------------------------Суммирование цифр
+int Method1(int number)
+{
+    if(number<0) 
+        number=number*(-1);
+
+    int sum = number%10;
+
+    while(number!=0)
+    {
+        number=number/10;
+        sum = sum+number%10;
+    }
+return sum;
+}
+
+
+
+//----------------Ввод данных
+static int UserNumber(string messageToUser)
+{
+    Console.Write(messageToUser);
+    try
+    {
+        return int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
